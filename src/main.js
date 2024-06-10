3c60d3561c2a46974b854f6c04cabeccea3be8fc
Alessandro Lo Verde <alessandro.loverde@gmail.com>
2024-06-10 13:05:06 +0200

    Attempt to implement Lottie

--- src/main.js
+++ src/main.js
@@ -5,12 +5,8 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from "./router";
 
-
 const app = createApp(App)
-
 //app.use(createPinia())
-
 //app.mount('#app')
-
 
 app.use(router).mount("#app");
